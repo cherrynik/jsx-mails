6a769ec5d34c77dfa3d04e88938217051280e267
Nik Cherednik <ighosta9@gmail.com>
2024-09-23 18:27:03 +0300

    chore: showcase of js code calling on markup render

--- src/markup/Welcome.tsx
+++ src/markup/Welcome.tsx
@@ -1,9 +1,11 @@
 // Must follow email client limitations (no CSS-in-JS or JavaScript interactions)
+const calculate = (a: number, b: number) => a + b;
+
 export const Welcome = () => {
   return (
     <>
       <h1>Hello</h1>
-      <p>Welcome to our service. We’re excited to have you!</p>
+      <p>Welcome to our service. {calculate(1, 3)}</p>
     </>
   );
 };
